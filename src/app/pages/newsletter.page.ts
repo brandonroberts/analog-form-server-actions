d289260ac1e02d734ad12059bf08b45e1ea01308
Brandon Roberts <robertsbt@gmail.com>
2024-12-05 13:11:52 -0600

    feat: add newsletter signup page example

---
+++ src/app/pages/newsletter.page.ts
@@ -0,0 +1,50 @@
+import { Component, signal } from '@angular/core';
+
+import { FormAction } from '@analogjs/router';
+
+type FormErrors =
+  | {
+    email?: string;
+  }
+  | undefined;
+
+@Component({
+  selector: 'app-newsletter-page',
+  imports: [FormAction],
+  template: `
+    <h3>Newsletter Signup</h3>
+
+    @if (!signedUp()) {
+      <form method="post"
+        (onSuccess)="onSuccess()"
+        (onError)="onError($any($event))"
+        (onStateChange)="errors.set(undefined)"
+      >
+        <div>
+          <label for="email"> Email </label>
+          <input type="email" name="email" autocomplete="off" />
+        </div>
+
+        <button class="button" type="submit">Submit</button>
+      </form>
+
+      @if( errors()?.email ) {
+        <p>{{ errors()?.email }}</p>
+      } 
+    } @else {
+      <div>Thanks for signing up!</div>
+    }
+  `,
+})
+export default class NewsletterComponent {
+  signedUp = signal(false);
+  errors = signal<FormErrors>(undefined);
+
+  onSuccess() {
+    this.signedUp.set(true);
+  }
+
+  onError(result?: FormErrors) {
+    this.errors.set(result);
+  }
+}
---
+++ src/app/pages/newsletter.server.ts
@@ -0,0 +1,18 @@
+import {
+  type PageServerAction,
+  json,
+  fail,
+  redirect,
+} from '@analogjs/router/server/actions';
+import { readFormData } from 'h3';
+
+export async function action({ event }: PageServerAction) {
+  const body = await readFormData(event);
+  const email = body.get('email') as string;
+
+  if (!email) {
+    return fail(422, { email: 'Email is required' });
+  }
+
+  return redirect('/');
+}
